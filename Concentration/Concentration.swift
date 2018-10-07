39bddb7976db54a226cf0bffda8dd3951649828e
Alfredo V <alfredo.villagomez.m@gmail.com>
2018-10-07 10:59:48 -0400

    Small changes

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-class Concentration {
+struct Concentration {
     
     private(set) var cards = [Card]()
     
@@ -43,11 +43,11 @@ class Concentration {
         // TODO: Shuffle the card
     }
     
-    func chooseCard(at index: Int) {
+    mutating func chooseCard(at index: Int) {
         assert(cards.indices.contains(index), "Concentration.chooseCard(at: \(index)): choosen index not in the cards")
         if !cards[index].isMatched {
             if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
-                if cards[matchIndex].identifier == cards[index].identifier {
+                if cards[matchIndex] == cards[index] {
                     cards[matchIndex].isMatched = true
                     cards[index].isMatched = true
                 }
--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -51,13 +51,13 @@ class ViewController: UIViewController {
     }
     
     private var emojiChoices = ["🎃", "👻", "🦇", "🙀", "🍭", "🍬", "🍎", "😱", "😈"]
-    private var emoji = [Int : String]()
+    private var emoji = [Card : String]()
     
     private func emoji(for card: Card) -> String {
-        if emoji[card.identifier] == nil, emojiChoices.count > 0 {
-            emoji[card.identifier] = emojiChoices.remove(at: emojiChoices.count.arc4random)
+        if emoji[card] == nil, emojiChoices.count > 0 {
+            emoji[card] = emojiChoices.remove(at: emojiChoices.count.arc4random)
         }
-        return emoji[card.identifier] ?? "/"
+        return emoji[card] ?? "/"
     }
 }
 
